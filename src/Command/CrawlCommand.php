970e387e90225643dc34d8570afb12a213cbe1bb
Evgeniy Bordiuk <ev.bordiuk@gmail.com>
2023-03-15 13:58:35 +0200

    Add options handling to worker.

--- src/Command/CrawlCommand.php
+++ src/Command/CrawlCommand.php
@@ -19,81 +19,78 @@ use App\Service\CrawlWorker;
 )]
 class CrawlCommand extends Command
 {
-  protected const VALID_URL_PATTERN = "/^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$/";
+    protected const VALID_URL_PATTERN = "/^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$/";
 
-  protected $targetUrl;
-  protected $options = [];
-  protected $io;
+    protected $targetUrl;
+    protected $options = [];
+    protected $io;
 
-  /**
-   * @inheritdoc
-   */
-  public function __construct(protected CrawlWorker $crawlWorker, protected RouterInterface $router)
-  {
-      parent::__construct();
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function initialize(InputInterface $input, OutputInterface $output): void
-  {
-    $this->targetUrl = $this->extractUrl($input->getArgument('url'));
-    $this->io = new SymfonyStyle($input, $output);
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function configure(): void
-  {
-    $this
-      ->addArgument('url', InputArgument::REQUIRED, 'Specify target url')
-      ->addOption('timeout', 't',  InputOption::VALUE_OPTIONAL, 'Stops script executing when time is reached')
-      ->addOption('deep', 'd',  InputOption::VALUE_OPTIONAL, 'Specify how deep links should be used')
-      ->addOption('pages', 'p',  InputOption::VALUE_OPTIONAL, 'Max number of parsed pages');
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-    if ($this->targetUrl == null) {
-      $this->io->error('Please, provide correct url address.');
-
-      return Command::INVALID;
+    /**
+     * @inheritdoc
+     */
+    public function __construct(protected CrawlWorker $crawlWorker, protected RouterInterface $router)
+    {
+        parent::__construct();
     }
 
-    try {
-      $this->crawlWorker->perform($this->targetUrl);
-    
-      $this->io->success(
-        sprintf(
-          'Task completed, please proceed to %s to see results',
-          $this->router->generate('app_results', [], UrlGeneratorInterface::ABSOLUTE_URL)
-        )
-      );
-
-      return Command::SUCCESS;
-    } catch (CrawlException | \Exception $e) {
-      $this->io->error($e->getMessage());
-
-      return Command::FAILURE;
+    /**
+     * @inheritdoc
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->targetUrl = $this->extractUrl($input->getArgument('url'));
+        $this->options   = array_filter($input->getOptions(), fn($option) => $option != null);
+        $this->io = new SymfonyStyle($input, $output);
     }
-  }
 
-  /**
-   * Extract url from string
-   * 
-   * @param string
-   * 
-   * @return string|null
-   */
-  protected function extractUrl($url): string|null
-  {
-    preg_match_all(CrawlCommand::VALID_URL_PATTERN, trim($url), $matches);
+    /**
+     * @inheritdoc
+     */
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('url', InputArgument::REQUIRED, 'Specify target url')
+            ->addOption('timeout', 't', InputOption::VALUE_OPTIONAL, 'Specify max number of seconds for script execution')
+            ->addOption('deep', 'd', InputOption::VALUE_OPTIONAL, 'Specify how deep links should be used')
+            ->addOption('pages', 'p', InputOption::VALUE_OPTIONAL, 'Max number of parsed pages');
+    }
 
-    return count($matches[0]) ? $matches[0][0] : null;
-  }
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if ($this->targetUrl == null) {
+            $this->io->error('Please, provide correct url address.');
+
+            return Command::INVALID;
+        }
+
+        try {
+            $this->crawlWorker->perform($this->targetUrl, $this->options);
+            
+            $this->io->success(
+                sprintf(
+                    'Task completed, please proceed to %s to see results',
+                    $this->router->generate('app_results', [], UrlGeneratorInterface::ABSOLUTE_URL)
+                )
+            );
+
+            return Command::SUCCESS;
+        } catch (CrawlException | \Exception $e) {
+            $this->io->error($e->getMessage());
+
+            return Command::FAILURE;
+        }
+    }
+
+    /**
+     * Extract url from string
+     */
+    protected function extractUrl(string $url): string|null
+    {
+        preg_match_all(CrawlCommand::VALID_URL_PATTERN, trim($url), $matches);
+
+        return count($matches[0]) ? $matches[0][0] : null;
+    }
 }
--- src/Service/CrawlWorker.php
+++ src/Service/CrawlWorker.php
@@ -5,109 +5,178 @@ namespace App\Service;
 use Symfony\Component\DomCrawler\Crawler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Entity\Result;
 
 class CrawlWorker
 {
-  private $domain;
-  private $cssSelector = 'body img';
-  private $urlList = [];
-  private $options = [];
+    private const INVALID_EXTENSIONS = '/\.js|\.ts|\.gz|#/';
 
-  public function __construct(private EntityManagerInterface $entityManager)
-  {
-  }
+    private $domain;
+    private $cssSelector = 'body img';
+    private $processedUrls = [];
+    private $requestOptions = [];
+    private $pageMax;
 
-  /**
-   * 
-   */
-  public function perform($targetUrl, $options): void
-  {
-    $this->options = $options;
-    $this->domain = parse_url($targetUrl, PHP_URL_HOST);
-    $this->addTarget($targetUrl);
-    $this->proceed();
-  }
-
-  private function proceed()
-  {
-    foreach ($this->getNextUrl() as $url) {
-      $stopwatch = new Stopwatch();
-      $stopwatch->start('crawling');
-      $occuranciesCount = $this->handleData($url);
-      $event = $stopwatch->stop('crawling');
-
-      $this->saveResults($url, $occuranciesCount, $event);
-    }
-  }
-
-  /**
-   * 
-   */
-  private function addTarget($url):void
-  {
-    $this->urlList[] = $url;
-  }
-
-  /**
-   * 
-   */
-  private function getNextUrl()
-  {
-    while ($url = current($this->urlList)) {
-      next($this->urlList);
-      yield $url;
-    }
-  }
-
-  /**
-   * 
-   */
-  private function handleData($targetUrl): int
-  {
-    $crawler = new Crawler($this->getHtml($targetUrl), null, $targetUrl);
-    $occuranciesCount = 0;
-
-    foreach ($crawler->filter($this->cssSelector)->images() as $image) {
-      if ($this->hostedByDomain($image)) {
-        $occuranciesCount++;
-      }
+    /**
+     * @inheritdoc
+     */
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface $httpClient
+    ) {
     }
 
-    return $occuranciesCount;
-  }
+    /**
+     * Start crawling
+     */
+    public function perform(string $targetUrl, array $options = []): void
+    {
+        $this->pageMax = $options['pages'] ?? null;
+        $this->domain = parse_url($targetUrl, PHP_URL_HOST);
 
-  /**
-   * 
-   */
-  private function saveResults($targetUrl, $occuranciesCount, $event)
-  {
-    $result = new Result();
-    $result->setUrl($targetUrl);
-    $result->setImagesTotal($occuranciesCount);
-    $result->setTimeSpent($event->getDuration()); // miliseconds
-    $this->entityManager->persist($result);
-    $this->entityManager->flush();
-  }
+        if (array_key_exists('timeout', $options)) {
+            $this->requestOptions['timeout'] = $options['timeout'];
+        }
 
-  /**
-   * Visit url and get html content
-   */
-  private function getHtml($targetUrl): string
-  {
-    return file_get_contents($targetUrl);
-  }
+        $this->proceed($targetUrl, $options['deep'] ?? 0);
+    }
 
-  /**
-   * Check if image hosts on the same domain
-   * We do not check relative pathes, because they all become absolute
-   * 
-   * @param Symfony\Component\DomCrawler\Image $image
-   * 
-   * @return bool
-   */
-  protected function hostedByDomain($image): bool
-  {
-    return strstr($image->getUri(), $this->domain);
-  }
+    /**
+     * Dispatch chain of actions - handle single page and start walking through child links
+     */
+    private function proceed(string $url, int $deep = 0)
+    {
+        $crawler = $this->buildCrawler($url);
+        $this->handlePage($crawler, $url);
+
+        if ($deep <= 0) {
+            return;
+        }
+        
+        return $this->proceedDeeper($crawler, $deep - 1);
+    }
+
+    /**
+     * Makes page's calculations
+     */
+    private function handlePage(Crawler $crawler, string $url): void
+    {
+        $stopwatch = new Stopwatch();
+        $occuranciesCount = 0;
+    
+        $stopwatch->start('crawling');
+
+        foreach ($crawler->filter($this->cssSelector)->images() as $image) {
+            if ($this->belongsToDomain($image->getUri())) {
+                $occuranciesCount++;
+            }
+        }
+
+        $event = $stopwatch->stop('crawling');
+
+        $this->saveResult($url, $occuranciesCount, $event);
+        $this->processedUrls[] = $url;
+    }
+
+    /**
+     * Follow for every child link
+     */
+    private function proceedDeeper(Crawler $crawler, int $deep = 0): void
+    {
+        foreach ($this->fetchInternalLinks($crawler) as $link) {
+            $this->proceed($link, $deep);
+        }
+    }
+
+    /**
+     * Fetch related link or break execution
+     */
+    private function fetchInternalLinks(Crawler $crawler): \Generator
+    {
+        $selector = 'body a';
+
+        foreach ($crawler->filter($selector)->links() as $link) {
+            if ($this->pagesMaxExceeded()) {
+                break;    
+            }
+
+            $url = $link->getUri();
+            
+            if ($this->parsed($url) || !$this->belongsToDomain($url)) {
+                continue;
+            }
+
+            yield $url;
+        }
+    }
+
+    /**
+     * Check if pages maximum exceeded
+     */
+    private function pagesMaxExceeded(): bool
+    {
+        if ($this->pageMax !== null) {
+            return $this->pageMax <= count($this->processedUrls);
+        }
+
+        return false;
+    }
+
+    /**
+     * Save Result record
+     */
+    private function saveResult(
+        string $url,
+        int $occuranciesCount,
+        \Symfony\Component\Stopwatch\StopwatchEvent $event
+    ): void {
+        $result = new Result();
+        $result->setUrl($url);
+        $result->setImagesTotal($occuranciesCount);
+        $result->setTimeSpent($event->getDuration()); // miliseconds
+        $this->entityManager->persist($result);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * Visit url and get html content
+     */
+    private function getHtml(string $url, array $options = []): string
+    {
+        $options = array_merge($this->requestOptions, $options);
+        $response = $this->httpClient->request('GET', $url, $options);
+
+        if ($response->getStatusCode() === 200) {
+            return $response->getContent();
+        }
+
+        return '';
+    }
+
+    /**
+     * Check if links are valid and leads to the same domain
+     */
+    private function belongsToDomain(string $url): bool
+    {
+        return !preg_match(CrawlWorker::INVALID_EXTENSIONS, $url) &&
+               preg_match("/(\/\/|\.)$this->domain/", $url);
+    }
+
+    /**
+     * Build Crawler instance
+     */
+    private function buildCrawler(string $url): Crawler
+    {
+        return new Crawler($this->getHtml($url), null, $url);
+    }
+
+    /**
+     * Verify if url is already parsed
+     */
+    private function parsed(string $url): bool
+    {
+        return in_array($url, $this->processedUrls) ||
+               in_array($url . '/', $this->processedUrls);
+    }
 }
